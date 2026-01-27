b7e5bae482a16c1fcd600ab15b37b04a0bfaca01
Chris Southam <chris@cjsoutham.com>
2026-01-27 22:13:12 +0000

    Initial commit

---
+++ src/ThemesServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TentaPress\Themes;
+
+use Illuminate\Support\ServiceProvider;
+
+final class ThemesServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'tentapress-themes');
+        $this->loadRoutesFrom(__DIR__.'/../routes/admin.php');
+    }
+}
